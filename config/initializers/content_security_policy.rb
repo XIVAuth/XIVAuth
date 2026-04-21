b038dda0bad1f34b8e053b4cf63def9d570270ba
Kaz Wolfe <root@kazwolfe.io>
2026-04-20 23:11:50 -0700

    chore: icons

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
     policy.font_src    :self, :https, :data
     policy.img_src     :self, :https, :data, :blob
     policy.object_src  :none
-    policy.script_src  :self, "https://challenges.cloudflare.com/", "https://static.cloudflareinsights.com/"
+    policy.script_src  :self, "https://challenges.cloudflare.com/", "https://*.cloudflareinsights.com/"
     policy.style_src   :self, :https, :unsafe_inline
 
     if (csp_base_uri = Rails.application.credentials.dig(:sentry, :csp_report_uri))
