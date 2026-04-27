cb3589568e46ce7318ee1918935c64b41063c759
Kaz Wolfe <root@kazwolfe.io>
2026-04-27 16:35:01 -0700

    fix: floating point precision

--- app/controllers/marketing_controller.rb
+++ app/controllers/marketing_controller.rb
@@ -7,7 +7,7 @@ class MarketingController < ApplicationController
       users: User.count,
       characters: CharacterRegistration.verified.count,
       applications: ClientApplication.count,
-      verification_rate: CharacterRegistration.verified.count.to_f / FFXIV::Character.count * 100
+      verification_rate: (CharacterRegistration.verified.count.to_f / FFXIV::Character.count * 100).round(1)
     }
 
     render :index
