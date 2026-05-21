91377bf3beaefc4088e037b9f56d5c5289b352e2
Kaz Wolfe <root@kazwolfe.io>
2026-05-21 11:20:37 -0700

    fix: team icons not showing

--- app/models/team/profile.rb
+++ app/models/team/profile.rb
@@ -2,8 +2,4 @@ class Team::Profile < ApplicationRecord
   self.primary_key = :team_id
 
   belongs_to :team, class_name: "Team", inverse_of: :profile
-
-  def avatar_url
-    super || "https://api.dicebear.com/9.x/icons/png?seed=#{team.name}"
-  end
 end
