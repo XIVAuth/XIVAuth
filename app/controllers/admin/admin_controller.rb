e955248a21b97e510bd0ad7c73954fc58ea2c687
Kaz Wolfe <root@kazwolfe.io>
2026-05-30 15:31:01 -0700

    feat: more SEO tags

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,9 +1,17 @@
 class Admin::AdminController < ApplicationController
   before_action :authorize_admin!
+  before_action :set_default_meta_tags
   layout "portal/page"
 
   def authorize_admin!
     # n.b. this should already be handled by the routing layer, but we'll put this here for safety's sake as well.
     raise ActionController::RoutingError, "Not Found" unless current_user.admin?
+  end
+
+  private def set_default_meta_tags
+    set_meta_tags site: "XIVAuth Admin",
+                  icon: [
+                    { href: helpers.asset_path('logos/xivauth-key-45-red.svg'), type: 'image/svg+xml', sizes: 'any' },
+                  ]
   end
 end
