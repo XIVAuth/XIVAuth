fd0ffc46376276c576b54694918794a63afb8d82
Kaz Wolfe <root@kazwolfe.io>
2026-06-12 12:41:32 -0700

    fix: rollback preferences code since it keeps being upset

--- app/models/user/preferences.rb
+++ app/models/user/preferences.rb
@@ -1,14 +1,6 @@
 class User::Preferences
   include StoreModel::Model
 
-  enum :theme, %i[auto astral umbral eulmore ishgard gridania thanalan limsa thirteenth]
-  enum :pride_flag, PrideHelper::PRIDE_FLAGS + %i[random none]
-  
-  def theme
-    self[:theme].nil? ? :auto : super
-  end
-  
-  def pride_flag
-    self[:pride_flag].nil? ? :random : super
-  end
+  enum :theme, %i[auto astral umbral eulmore ishgard gridania thanalan limsa thirteenth], default: :auto
+  enum :pride_flag, PrideHelper::PRIDE_FLAGS + %i[random none], default: :random
 end
