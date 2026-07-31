465e1f4750de486d07f728e2821f088e112d9b81
Kaz Wolfe <root@kazwolfe.io>
2026-07-30 18:58:54 -0700

    feat: add "not you" link to consent screen

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -173,4 +173,13 @@ class Users::SessionsController < Devise::SessionsController
 
     stored_location_for(resource) || character_registrations_path
   end
+
+  def after_sign_out_path_for(resource_or_scope)
+    return_to = params[:redirect_to]
+    if return_to.present?
+      return_to
+    else
+      super
+    end
+  end
 end
